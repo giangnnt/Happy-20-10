c9cddadbce3ae7be1e203e8c2a6406275eb7e53d
GiangNNT <giangnntse173469@fpt.edu.vn>
2023-10-19 19:08:26 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Vânn Annh';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100034507645451';
+const eventName = '20-10 zui zẻ đêys';
+const titleCard = 'Anh tặng Annh';
+const contentCard = 'Ngày hôm này tui gửi Vânn Annh lời chúc tốt đẹp nhất. Hy vọng bạn luôn giữ niềm tin, sự tự tin và mạnh mẽ. Chúc bạn có một ngày thật hạnh phúc và đầy ý nghĩa 😘';
+const giftImage = 'VA.jpg';
+const giftImage1 = 'VA1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
